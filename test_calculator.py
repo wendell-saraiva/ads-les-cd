c81d542072427a7bd6b962b9bbc32ca7d8bbfd26
Ricardo Job <sousajob@gmail.com>
2026-06-09 10:37:17 -0300

    first commit

---
+++ test_calculator.py
@@ -0,0 +1,18 @@
+import pytest
+from calculator import *
+
+def test_soma():
+    assert soma(2, 3) == 5
+
+
+def test_subtracao():
+    assert subtracao(10, 4) == 6
+
+
+def test_multiplicacao():
+    assert multiplicacao(3, 4) == 12
+
+
+def test_divisao():
+    assert divisao(8, 2) == 4
+
